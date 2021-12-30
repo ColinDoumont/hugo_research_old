8580ebb9324403c759ea1eb908f9a9fea28b3c06
ColinDoumont <colin@principiae.be>
2021-12-30 18:55:41 +0100

    Rewrote preprocessing.ipynb as 3 different files.

---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,77 @@
+"""
+Functions used for pre-processing the data.
+All functions follow a simple pattern:
+    input = df
+    output = transformed_df
+"""
+
+
+# Function to fill in missing values in a forward manner.
+def ffill(df, columns_list, id='cusip'):
+    """
+    type(df) = pandas DataFrame
+    type(columns_list) = list
+    type(id) = str
+    """
+    df_ffill = df.copy()
+
+    for column in columns_list:
+        df_ffill[column] = (df_ffill.groupby(id)[column].ffill())
+
+    return df_ffill
+
+
+# Function to fill in missing values by taking median of cross-section.
+def mfill(df, columns_list, date='datadate'):
+    """
+    type(df) = pandas DataFrame
+    type(columns_list) = list
+    type(dates) = str
+    """
+    df_mfill = df.copy()
+
+    for column in columns_list:
+        df_mfill[column] = df_mfill[column].fillna(df_mfill.groupby(date)[column].transform('median'))
+
+    return df_mfill
+
+
+# Function to add lags in the dataframe.
+def lagged(df, columns_list, id='cusip', lags_list=[-5,-4,-3,-2,-1,1]):
+    """
+    type(df) = pandas DataFrame
+    type(columns_list) = list
+    type(id) = str
+    type (lags_list) = list
+    """
+    lags_list = [-i for i in lags_list]
+    df_lagged = df.copy()
+    X_col_list = columns_list.copy()
+    y_col_list = []
+
+    for lag in lags_list:
+        for column in columns_list:
+            new_col_name = column + str('_') + str(-lag)
+            df_lagged[new_col_name] = (df_lagged.groupby(id)[column].shift(lag))
+
+            if(lag < 0):
+                y_col_list.append(new_col_name)
+            else:
+                X_col_list.append(new_col_name)
+
+    return df_lagged, X_col_list, y_col_list
+
+
+# Function to convert quarterly data into trailing twelve months (TTM) data.
+def ttm(df, columns_list=['revtq', 'cogsq', 'xsgaq', 'niq', 'ebitq'], id='cusip', date='datadate'):
+    """
+    type(df) = pandas DataFrame
+    type(columns_list) = list
+    type(id) = str
+    type(date) = str
+    """
+    df_ttm = df.copy()
+    columns_list.append('datadate')
+    columns_list.append('cusip')
+    df_ttm[columns_list] = df_ttm.groupby('cusip')[columns_list].rolling('365D', on='datadate', min_periods=4).sum().reset_index()[columns_list]
+    return df_ttm
